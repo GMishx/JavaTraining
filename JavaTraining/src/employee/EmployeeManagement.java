b97caa49bc06158faa1edf8b776c8d00f7cb8bbe
Z003U7PX <Z003U7PX@AAEINBLR06680L.ad001.siemens.net>
2017-10-23 11:24:57 +0530

    Created Employee objects

---
+++ JavaTraining/src/employee/EmployeeManagement.java
@@ -0,0 +1,14 @@
+package employee;
+
+import employee.elements.*;
+
+public class EmployeeManagement {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee(1);
+		emp1.setName("Sri Ranga Sai");
+		emp1.setSalary(38000.00);
+		System.out.println(emp1);
+	}
+
+}
---
+++ JavaTraining/src/employee/elements/Employee.java
@@ -0,0 +1,80 @@
+package employee.elements;
+
+public class Employee {
+	private String name;
+	private final int id;
+	private double salary;
+
+	public Employee(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Employee)) {
+			return false;
+		}
+		Employee other = (Employee) obj;
+		if (id != other.id) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ID: " + this.getId());
+		sb.append(", Name: " + this.getName());
+		sb.append(", Salary: " + this.getSalary());
+		return sb.toString();
+	}
+}
